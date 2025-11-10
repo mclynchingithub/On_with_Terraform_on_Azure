de7e19edfb006c4981423f60944d195d24c335ac
Chris Lynch <clynch@uncomn.com>
2025-11-10 17:55:30 -0600

    Test

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  cloud {
+    organization = "clynch-pluralsight-lab"
+    workspaces {
+      name = "Hands-On_With_Terraform_On_Azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-15ef01b4-hands-on-with-terraform-on-azure"
+  location = "westus"
+}
+
+module "securestorage" {
+  source               = "app.terraform.io/clynch-pluralsight-lab/securestorage2/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "tsstx123handonwithtfaz"
+}
