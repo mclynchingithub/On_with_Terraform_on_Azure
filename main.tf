38336a83327e92bf4deaac842b26e7c8a889df01
Chris Lynch <clynch@uncomn.com>
2025-11-10 18:22:03 -0600

    test4

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
   skip_provider_registration = true
 }
 
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "ARM_SUBSCRIPTION_ID" {
+      description = "The client secret for the Azure service principal."
+      type        = string
+      sensitive   = true # Mark as sensitive if it contains confidential information
+    }
+variable "ARM_TENANT_ID" {
+      description = "The client secret for the Azure service principal."
+      type        = string
+      sensitive   = true # Mark as sensitive if it contains confidential information
+    }
+variable "ARM_CLIENT_ID" {
+      description = "The client secret for the Azure service principal."
+      type        = string
+      sensitive   = true # Mark as sensitive if it contains confidential information
+    }
+variable "ARM_CLIENT_SECRET" {
+      description = "The client secret for the Azure service principal."
+      type        = string
+      sensitive   = true # Mark as sensitive if it contains confidential information
+    }
