f400e9c90e3fa1279b03d92deebe57dbc5c673f2
Chris Lynch <clynch@uncomn.com>
2025-11-10 18:05:17 -0600

    test2

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "rg" {
 
 module "securestorage" {
   source               = "app.terraform.io/clynch-pluralsight-lab/securestorage2/azurerm"
-  version              = "1.0.0"
+  version              = "v1.0.0"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
   storage_account_name = "tsstx123handonwithtfaz"
