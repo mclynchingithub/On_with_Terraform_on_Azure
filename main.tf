af05a86c5ae04d39b08e20e8c331c7e77ba0fb92
Chris Lynch <clynch@uncomn.com>
2025-11-13 18:00:50 -0600

    blah blah

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
+  subscription_id            = "9734ed68-621d-47ed-babd-269110dbacb1"
   skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "813-15ef01b4-hands-on-with-terraform-on-azure"
+  name     = "813-08022121-hands-on-with-terraform-on-azure"
   location = "westus"
 }
 
@@ -23,5 +23,5 @@ module "securestorage" {
   version              = "1.0.0"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_name = "tsstx123handonwithtfaz"
+  storage_account_name = "tsstx124handonwithtfaz"
 }
